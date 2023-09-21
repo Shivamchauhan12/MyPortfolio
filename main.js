d5927b8755958ea72bda72348156a2b81bd27d0c
Shivamchauhan12 <shivamchauhan57332@gmail.com>
2023-09-21 18:15:48 +0530

    add

---
+++ main.js
@@ -0,0 +1,8 @@
+  var typed=new Typed(".text",{
+    strings:["A computer Science Student","A web Designer","A Frontend Developer", "and a Good Athlete"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop:true
+  });
+  
